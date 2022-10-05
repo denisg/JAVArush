7c312e2afe6dd0dcbaf69794852bfc320dab8fac
afernandez <afernandez@excelsis.com.py>
2022-10-05 11:06:48 -0300

    uso de paquetes

---
+++ Paquetes/src/py/com/JAVArush/Utileria.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package py.com.JAVArush;
+
+
+public class Utileria {
+    public static void imprimir(String texto) {
+        System.out.println("texto = " + texto);
+    }
+}
---
+++ Paquetes/src/test/UtileriaTest.java
@@ -0,0 +1,15 @@
+
+package test;
+
+//import py.com.JAVArush.Utileria;//import de forma normal
+import static py.com.JAVArush.Utileria.imprimir;  //importa pero solo atributos y metodos estaticos
+
+public class UtileriaTest {
+    
+    public static void main(String[] args) {
+        //Utileria.imprimir("up denis");
+        
+        imprimir("up denis");//asi de forma directa se utiliza la funcion que se importo de otro paquete
+    }
+    
+}
